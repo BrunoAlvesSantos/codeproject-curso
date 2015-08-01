c6bad0452656acc7319fe19b622e66957ce990e5
Bruno Alves <brunoalvesbr@gmail.com.br>
2015-08-01 00:04:22 -0300

    Client Validator

---
+++ app/Services/ClientService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brunoasantos
+ * Date: 31/07/15
+ * Time: 23:28
+ */
+
+namespace CodeProject\Services;
+
+
+class ClientService {
+
+}
---
+++ app/Validators/ClientValidator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brunoasantos
+ * Date: 31/07/15
+ * Time: 23:41
+ */
+
+namespace CodeProject\Validators;
+
+
+class ClientValidator {
+
+}
